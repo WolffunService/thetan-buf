6e396fa2b4888fcd84ec3aad13ca1e21cf6e2f6d
sanvuqui <108712804+sanvuqui@users.noreply.github.com>
2023-06-26 16:38:43 +0700

    update proto matchfound with fishnet info

--- gen/go/thetan/gateway/v1/gs_sync.pb.go
+++ gen/go/thetan/gateway/v1/gs_sync.pb.go
@@ -344,6 +344,62 @@ func (x *Immortal_RoomAllocationResp) GetRoomId() string {
 	return ""
 }
 
+// nên đặt bên proto của match-director nhưng để đây để tránh import cycle
+type Immortal_MatchFoundForMultiplayer struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	RoomAlloc *Immortal_RoomAllocationResp `protobuf:"bytes,1,opt,name=roomAlloc,proto3" json:"roomAlloc,omitempty"`
+	PartyIDs  []string                     `protobuf:"bytes,2,rep,name=partyIDs,proto3" json:"partyIDs,omitempty"`
+}
+
+func (x *Immortal_MatchFoundForMultiplayer) Reset() {
+	*x = Immortal_MatchFoundForMultiplayer{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_thetan_gateway_v1_gs_sync_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Immortal_MatchFoundForMultiplayer) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Immortal_MatchFoundForMultiplayer) ProtoMessage() {}
+
+func (x *Immortal_MatchFoundForMultiplayer) ProtoReflect() protoreflect.Message {
+	mi := &file_thetan_gateway_v1_gs_sync_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Immortal_MatchFoundForMultiplayer.ProtoReflect.Descriptor instead.
+func (*Immortal_MatchFoundForMultiplayer) Descriptor() ([]byte, []int) {
+	return file_thetan_gateway_v1_gs_sync_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *Immortal_MatchFoundForMultiplayer) GetRoomAlloc() *Immortal_RoomAllocationResp {
+	if x != nil {
+		return x.RoomAlloc
+	}
+	return nil
+}
+
+func (x *Immortal_MatchFoundForMultiplayer) GetPartyIDs() []string {
+	if x != nil {
+		return x.PartyIDs
+	}
+	return nil
+}
+
 var File_thetan_gateway_v1_gs_sync_proto protoreflect.FileDescriptor
 
 var file_thetan_gateway_v1_gs_sync_proto_rawDesc = []byte{
@@ -384,7 +440,16 @@ var file_thetan_gateway_v1_gs_sync_proto_rawDesc = []byte{
 	0x70, 0x12, 0x1f, 0x0a, 0x0b, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5f, 0x70, 0x6f, 0x72, 0x74,
 	0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x0a, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x50, 0x6f,
 	0x72, 0x74, 0x12, 0x17, 0x0a, 0x07, 0x72, 0x6f, 0x6f, 0x6d, 0x5f, 0x69, 0x64, 0x18, 0x03, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x06, 0x72, 0x6f, 0x6f, 0x6d, 0x49, 0x64, 0x32, 0xb5, 0x03, 0x0a, 0x0d,
+	0x01, 0x28, 0x09, 0x52, 0x06, 0x72, 0x6f, 0x6f, 0x6d, 0x49, 0x64, 0x22, 0x8d, 0x01, 0x0a, 0x21,
+	0x49, 0x6d, 0x6d, 0x6f, 0x72, 0x74, 0x61, 0x6c, 0x5f, 0x4d, 0x61, 0x74, 0x63, 0x68, 0x46, 0x6f,
+	0x75, 0x6e, 0x64, 0x46, 0x6f, 0x72, 0x4d, 0x75, 0x6c, 0x74, 0x69, 0x70, 0x6c, 0x61, 0x79, 0x65,
+	0x72, 0x12, 0x4c, 0x0a, 0x09, 0x72, 0x6f, 0x6f, 0x6d, 0x41, 0x6c, 0x6c, 0x6f, 0x63, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x0b, 0x32, 0x2e, 0x2e, 0x74, 0x68, 0x65, 0x74, 0x61, 0x6e, 0x2e, 0x67, 0x61,
+	0x74, 0x65, 0x77, 0x61, 0x79, 0x2e, 0x76, 0x31, 0x2e, 0x49, 0x6d, 0x6d, 0x6f, 0x72, 0x74, 0x61,
+	0x6c, 0x5f, 0x52, 0x6f, 0x6f, 0x6d, 0x41, 0x6c, 0x6c, 0x6f, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e,
+	0x52, 0x65, 0x73, 0x70, 0x52, 0x09, 0x72, 0x6f, 0x6f, 0x6d, 0x41, 0x6c, 0x6c, 0x6f, 0x63, 0x12,
+	0x1a, 0x0a, 0x08, 0x70, 0x61, 0x72, 0x74, 0x79, 0x49, 0x44, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28,
+	0x09, 0x52, 0x08, 0x70, 0x61, 0x72, 0x74, 0x79, 0x49, 0x44, 0x73, 0x32, 0xb5, 0x03, 0x0a, 0x0d,
 	0x54, 0x68, 0x65, 0x74, 0x61, 0x6e, 0x47, 0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x12, 0x49, 0x0a,
 	0x04, 0x50, 0x69, 0x6e, 0x67, 0x12, 0x1e, 0x2e, 0x74, 0x68, 0x65, 0x74, 0x61, 0x6e, 0x2e, 0x67,
 	0x61, 0x74, 0x65, 0x77, 0x61, 0x79, 0x2e, 0x76, 0x31, 0x2e, 0x50, 0x69, 0x6e, 0x67, 0x52, 0x65,
@@ -439,7 +504,7 @@ func file_thetan_gateway_v1_gs_sync_proto_rawDescGZIP() []byte {
 	return file_thetan_gateway_v1_gs_sync_proto_rawDescData
 }
 
-var file_thetan_gateway_v1_gs_sync_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_thetan_gateway_v1_gs_sync_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_thetan_gateway_v1_gs_sync_proto_goTypes = []interface{}{
 	(*PingRequest)(nil),                        // 0: thetan.gateway.v1.PingRequest
 	(*PingResponse)(nil),                       // 1: thetan.gateway.v1.PingResponse
@@ -447,22 +512,24 @@ var file_thetan_gateway_v1_gs_sync_proto_goTypes = []interface{}{
 	(*PlayerDisconnectedRequest)(nil),          // 3: thetan.gateway.v1.PlayerDisconnectedRequest
 	(*PlayerStatusResponse)(nil),               // 4: thetan.gateway.v1.PlayerStatusResponse
 	(*Immortal_RoomAllocationResp)(nil),        // 5: thetan.gateway.v1.Immortal_RoomAllocationResp
-	(*v1.ImmortalMatchFoundResponseProto)(nil), // 6: thetan.immortal.v1.ImmortalMatchFoundResponseProto
+	(*Immortal_MatchFoundForMultiplayer)(nil),  // 6: thetan.gateway.v1.Immortal_MatchFoundForMultiplayer
+	(*v1.ImmortalMatchFoundResponseProto)(nil), // 7: thetan.immortal.v1.ImmortalMatchFoundResponseProto
 }
 var file_thetan_gateway_v1_gs_sync_proto_depIdxs = []int32{
-	0, // 0: thetan.gateway.v1.ThetanGateway.Ping:input_type -> thetan.gateway.v1.PingRequest
-	2, // 1: thetan.gateway.v1.ThetanGateway.PlayerConnected:input_type -> thetan.gateway.v1.PlayerConnectedRequest
-	3, // 2: thetan.gateway.v1.ThetanGateway.PlayerDisconnected:input_type -> thetan.gateway.v1.PlayerDisconnectedRequest
-	6, // 3: thetan.gateway.v1.ThetanGateway.Immortal_RoomAllocation:input_type -> thetan.immortal.v1.ImmortalMatchFoundResponseProto
-	1, // 4: thetan.gateway.v1.ThetanGateway.Ping:output_type -> thetan.gateway.v1.PingResponse
-	4, // 5: thetan.gateway.v1.ThetanGateway.PlayerConnected:output_type -> thetan.gateway.v1.PlayerStatusResponse
-	4, // 6: thetan.gateway.v1.ThetanGateway.PlayerDisconnected:output_type -> thetan.gateway.v1.PlayerStatusResponse
-	5, // 7: thetan.gateway.v1.ThetanGateway.Immortal_RoomAllocation:output_type -> thetan.gateway.v1.Immortal_RoomAllocationResp
-	4, // [4:8] is the sub-list for method output_type
-	0, // [0:4] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	5, // 0: thetan.gateway.v1.Immortal_MatchFoundForMultiplayer.roomAlloc:type_name -> thetan.gateway.v1.Immortal_RoomAllocationResp
+	0, // 1: thetan.gateway.v1.ThetanGateway.Ping:input_type -> thetan.gateway.v1.PingRequest
+	2, // 2: thetan.gateway.v1.ThetanGateway.PlayerConnected:input_type -> thetan.gateway.v1.PlayerConnectedRequest
+	3, // 3: thetan.gateway.v1.ThetanGateway.PlayerDisconnected:input_type -> thetan.gateway.v1.PlayerDisconnectedRequest
+	7, // 4: thetan.gateway.v1.ThetanGateway.Immortal_RoomAllocation:input_type -> thetan.immortal.v1.ImmortalMatchFoundResponseProto
+	1, // 5: thetan.gateway.v1.ThetanGateway.Ping:output_type -> thetan.gateway.v1.PingResponse
+	4, // 6: thetan.gateway.v1.ThetanGateway.PlayerConnected:output_type -> thetan.gateway.v1.PlayerStatusResponse
+	4, // 7: thetan.gateway.v1.ThetanGateway.PlayerDisconnected:output_type -> thetan.gateway.v1.PlayerStatusResponse
+	5, // 8: thetan.gateway.v1.ThetanGateway.Immortal_RoomAllocation:output_type -> thetan.gateway.v1.Immortal_RoomAllocationResp
+	5, // [5:9] is the sub-list for method output_type
+	1, // [1:5] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_thetan_gateway_v1_gs_sync_proto_init() }
@@ -543,6 +610,18 @@ func file_thetan_gateway_v1_gs_sync_proto_init() {
 				return nil
 			}
 		}
+		file_thetan_gateway_v1_gs_sync_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Immortal_MatchFoundForMultiplayer); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -550,7 +629,7 @@ func file_thetan_gateway_v1_gs_sync_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_thetan_gateway_v1_gs_sync_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
