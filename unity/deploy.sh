3dcc681aa6bb3d821fb4ef532f8ca98dc85ff795
Phước Trung <trungdlp@wolffungame.com>
2023-07-10 19:10:30 +0700

    Release with package.json

--- unity/deploy.sh
+++ unity/deploy.sh
@@ -37,6 +37,7 @@ PREFIX="gen/csharp"
 BRANCH="upm"
 
 cp ./unity/package.json ./gen/csharp/package.json
+cp ./unity/Wolffun.Protobuf.asmdef ./gen/csharp/Wolffun.Protobuf.asmdef
 git add .
 git commit -m "Release with package.json"
 
