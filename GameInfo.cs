1f30e8ddc4228e51b0fd6459996c4d401ae13273
sanvuqui <sanvq@wolffungame.com>
2023-12-25 23:43:38 +0700

    add arena enum

--- GameInfo.cs
+++ GameInfo.cs
@@ -36,15 +36,16 @@ namespace Thetan.Shared.V1 {
             "EhEKDVRIRVRBTl9SSVZBTFMQFBIcChhUSEVUQU5fUklWQUxTX1RPVVJOQU1F",
             "TlQQFRIcChhUSEVUQU5fUklWQUxTX1FVSUNLX01PREUQFhIaChZUSEVUQU5f",
             "UklWQUxTX0RVT19NT0RFEBcSHwobVEhFVEFOX1JJVkFMU19DTElNQl9VUF9N",
-            "T0RFEBgSFgoJTk9ORV9NT0RFEP///////////wFCvAEKFGNvbS50aGV0YW4u",
-            "c2hhcmVkLnYxQg1HYW1lSW5mb1Byb3RvUAFaM3RoZXRhbi1idWYvZ2VuL2dv",
-            "L3RoZXRhbi9zaGFyZWQvdjE7dGhldGFuX3NoYXJlZF92MaICA1RTWKoCEFRo",
-            "ZXRhbi5TaGFyZWQuVjHKAhBUaGV0YW5cU2hhcmVkXFYx4gIcVGhldGFuXFNo",
-            "YXJlZFxWMVxHUEJNZXRhZGF0YeoCElRoZXRhbjo6U2hhcmVkOjpWMWIGcHJv",
-            "dG8z"));
+            "T0RFEBgSFgoJTk9ORV9NT0RFEP///////////wEqUAoHVFJBcmVuYRIICgRO",
+            "T05FEAASCgoGTk9WSUNFEAESCwoHRklHSFRFUhACEgsKB1dBUlJJT1IQAxIK",
+            "CgZNQVNURVIQBBIJCgVFTElURRAFQrwBChRjb20udGhldGFuLnNoYXJlZC52",
+            "MUINR2FtZUluZm9Qcm90b1ABWjN0aGV0YW4tYnVmL2dlbi9nby90aGV0YW4v",
+            "c2hhcmVkL3YxO3RoZXRhbl9zaGFyZWRfdjGiAgNUU1iqAhBUaGV0YW4uU2hh",
+            "cmVkLlYxygIQVGhldGFuXFNoYXJlZFxWMeICHFRoZXRhblxTaGFyZWRcVjFc",
+            "R1BCTWV0YWRhdGHqAhJUaGV0YW46OlNoYXJlZDo6VjFiBnByb3RvMw=="));
       descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
           new pbr::FileDescriptor[] { },
-          new pbr::GeneratedClrTypeInfo(new[] {typeof(global::Thetan.Shared.V1.GameMode), typeof(global::Thetan.Shared.V1.InGameMode), }, null, null));
+          new pbr::GeneratedClrTypeInfo(new[] {typeof(global::Thetan.Shared.V1.GameMode), typeof(global::Thetan.Shared.V1.InGameMode), typeof(global::Thetan.Shared.V1.TRArena), }, null, null));
     }
     #endregion
 
@@ -77,6 +78,15 @@ namespace Thetan.Shared.V1 {
     [pbr::OriginalName("THETAN_RIVALS_DUO_MODE")] ThetanRivalsDuoMode = 23,
     [pbr::OriginalName("THETAN_RIVALS_CLIMB_UP_MODE")] ThetanRivalsClimbUpMode = 24,
     [pbr::OriginalName("NONE_MODE")] NoneMode = -1,
+  }
+
+  public enum TRArena {
+    [pbr::OriginalName("NONE")] None = 0,
+    [pbr::OriginalName("NOVICE")] Novice = 1,
+    [pbr::OriginalName("FIGHTER")] Fighter = 2,
+    [pbr::OriginalName("WARRIOR")] Warrior = 3,
+    [pbr::OriginalName("MASTER")] Master = 4,
+    [pbr::OriginalName("ELITE")] Elite = 5,
   }
 
   #endregion
