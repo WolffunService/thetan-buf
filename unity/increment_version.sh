757348d3a0dce74dc646c7c708f45f1cac4fb30a
Tinh <63591119+tinh98@users.noreply.github.com>
2024-10-30 09:59:14 +0700

    auto tag

--- unity/increment_version.sh
+++ unity/increment_version.sh
@@ -12,4 +12,5 @@ new_version=$(echo "$current_version" | awk -F. -v OFS=. '{$NF++;print}')
 jq --arg new_version "$new_version" '.version = $new_version' package.json > tmp.json && mv tmp.json package.json
 
 echo "Version incremented to $new_version"
+export VERSION="$new_version"
 
