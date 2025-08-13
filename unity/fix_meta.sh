beb65d152f011dd0fc6fb0509eb12bf9ccc3b972
Hồ Công Thành <hothanh@wolffungame.com>
2025-08-13 14:25:29 +0700

    fix

--- unity/fix_meta.sh
+++ unity/fix_meta.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 ./tools/unity-meta-check ./gen/csharp | ./tools/unity-meta-autofix -root-dir ./gen/csharp .
-
-./tools/unity-meta-check ./gen/csharp/GRPC/ | ./tools/unity-meta-autofix -root-dir ./gen/csharp/GRPC/ .
