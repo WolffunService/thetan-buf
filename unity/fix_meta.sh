dfff603fb4dcfd940dfa76bf52117087b38ee3fa
Hồ Công Thành <hothanh@wolffungame.com>
2025-08-13 14:05:33 +0700

    Move grpc file into submodule

--- unity/fix_meta.sh
+++ unity/fix_meta.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ./tools/unity-meta-check ./gen/csharp | ./tools/unity-meta-autofix -root-dir ./gen/csharp .
+
+./tools/unity-meta-check ./gen/csharp/GRPC/ | ./tools/unity-meta-autofix -root-dir ./gen/csharp/GRPC/ .
